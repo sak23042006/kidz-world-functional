908138f490f2beecae0214ccb50bf57581b8872c
sak23042006 <138766433+sak23042006@users.noreply.github.com>
2023-10-18 09:45:55 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -124,19 +124,22 @@ function updatePrice() {
 cartButton.onclick = () => {
   updatePrice();
 
+  if(cartValue.innerText==0){
+    alert("Cart Empty")
+    return
+  }
+
+
+  var text=""
 
   for (let index = 0; index < items.length; index++) {
     if (items[index].quantity != 0) {
-      console.log(
-        "Item name: " +
-          items[index].name +
-          " - Quantity: " +
-          items[index].quantity
-      );
+      text+="Item name: " + items[index].name + " - Quantity: " + items[index].quantity + " " ;
     }
   }
 
-  console.log(
-    "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
-  );
+    text += "The total amount is " + finalDollars + "$ and " + finalCents + " cents" + " "
+
+    location.href=("https://wa.me/9384143079?text="+ text)
+
 };
